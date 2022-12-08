cd3d2354961e4d8a8be12b544f48fe6c6bac1354
Andrii Illiashenko <technolog2007@gmail.com>
2022-12-08 14:23:24 +0200

    50000 items in property

--- src/test/java/shpp/com/app/RequestToDBTest.java
+++ src/test/java/shpp/com/app/RequestToDBTest.java
@@ -5,16 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RequestToDBTest {
+    @Test
+    void addDocumentsToDB() {
+    }
 
     @Test
     void search() {
     }
 
     @Test
-    void addDocumentsToDB() {
-    }
-
-    @Test
-    void testSearch() {
+    void getTotalTime() {
     }
 }
